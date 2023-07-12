005da1e8f335a21c6c2888656feb2f12d3f31a55
roissi <cyrildegraeve@gmail.com>
2023-07-12 23:41:52 +0200

    sitemap added

--- my-portfolio/next-sitemap.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    siteUrl: 'https://portfolio-roissi.vercel.app',
-    generateRobotsTxt: true,
-    // optional
-    // robots.txt file is generated automatically
-    exclude: [],
-  }
