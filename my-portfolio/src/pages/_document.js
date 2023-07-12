cbbc5d55c75f35dfec96b740ac6c7a1860f39fc2
roissi <cyrildegraeve@gmail.com>
2023-07-12 11:57:24 +0200

    GA added

--- my-portfolio/src/pages/_document.js
+++ my-portfolio/src/pages/_document.js
@@ -5,8 +5,23 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          {/* Existing tags */}
           <link rel="icon" href="/favicon.ico" />
           <link href="https://fonts.googleapis.com/css2?family=Paytone+One&family=Outfit&display=swap" rel="stylesheet" />
+
+          {/* Google Analytics */}
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-DXFD92L858"></script>
+          <script dangerouslySetInnerHTML={
+            {
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-DXFD92L858');
+              `
+            }
+          }>
+          </script>
         </Head>
         <body>
           <Main />
