da72449c80bb59ee0adb244753b86cc8af489588
roissi <cyrildegraeve@gmail.com>
2023-11-10 00:32:17 +0100

    imageVariants

--- my-portfolio/next.config.js
+++ my-portfolio/next.config.js
@@ -1,10 +1,10 @@
-const { i18n } = require('./next-i18next.config');
+const { i18n } = require("./next-i18next.config");
 
 const nextConfig = {
   i18n: {
     ...i18n,
-    locales: ['en', 'fr'],
-    defaultLocale: 'fr',
+    locales: ["en", "fr"],
+    defaultLocale: "fr",
     localeDetection: false,
   },
   // ...autres configurations
